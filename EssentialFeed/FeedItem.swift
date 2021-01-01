1ff4df0f94a70879d4588324dc525d30730ba7d8
Ali Elsokary <ali.elsokary8@gmail.com>
2021-01-01 22:55:06 +0200

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ali Elsokary on 1/1/21.
+//  
+//
+
+import Foundation
+
+struct FeedItem {
+	let uuid: String
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
