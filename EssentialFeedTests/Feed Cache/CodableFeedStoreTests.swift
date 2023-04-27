d1ea480ba81efce874cfcf2b3175f65549e89ced
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-04-27 21:44:48 +0200

    Inserting to non-empty cache overrides previously inserted cache values

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -129,6 +129,19 @@ class CodableFeedStoreTests: XCTestCase {
 		expect(sut, toRetrieveTwice: .failure(anyNSError()))
 	}
 	
+	func insert_overridesPreviouslyInsertedCacheValues() {
+		let sut = makeSUT()
+		
+		let firstInsertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+		
+		let latestFeed = uniqueImageFeed().local
+		let latestTimestamp = Date()
+		
+		let latestInsertionError = insert((latestFeed, latestTimestamp), to: sut)
+		XCTAssertNil(latestInsertionError, "Expected to override cache successfully")
+		expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -136,14 +149,17 @@ class CodableFeedStoreTests: XCTestCase {
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}
-	
-	private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+
+	@discardableResult
+	private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) -> Error? {
 		let exp = expectation(description: "Wait for cache insertion")
-		sut.insert(cache.feed, timestamp: cache.timestamp, completion: { insertionError in
-			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+		var insertionError: Error?
+		sut.insert(cache.feed, timestamp: cache.timestamp, completion: { receivedInsertionError in
+			insertionError = receivedInsertionError
 			exp.fulfill()
 		})
 		wait(for: [exp], timeout: 1.0)
+		return insertionError
 	}
 
 	private func expect(_ sut: CodableFeedStore, toRetrieveTwice expectedResult: RetrieveCachedFeedResult, file: StaticString = #filePath, line: UInt = #line) {
