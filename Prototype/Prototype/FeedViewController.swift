6b5d85b7821351ac563184f6241aa8c00bd95def
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-05-11 20:18:27 +0300

    Add `UIRefreshControl` to prototype to simulate async loading of the feed

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -16,26 +16,44 @@ struct FeedImageViewModel {
 
 final class FeedViewController: UITableViewController {
 	
-	let feed = FeedImageViewModel.prototypeFeed
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	private var feed = [FeedImageViewModel]()
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		refresh()
+		tableView.setContentOffset(CGPoint(x: 0, y: -tableView.contentInset.top), animated: false)
+	}
+	
+	@IBAction func refresh() {
+		refreshControl?.beginRefreshing()
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+			if self.feed.isEmpty {
+				self.feed = FeedImageViewModel.prototypeFeed
+				self.tableView.reloadData()
+			}
+			self.refreshControl?.endRefreshing()
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return feed.count
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+	}
+	
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
 		let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
 		let model = feed[indexPath.row]
 		cell.configure(with: model)
 		return cell
-    }
-
+	}
+	
 }
 
 extension FeedImageCell {
