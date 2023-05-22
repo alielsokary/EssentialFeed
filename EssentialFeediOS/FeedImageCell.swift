5e2a1a506dd5bbe8157efccafd0d2bee47c3ac66
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-05-22 15:58:57 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Ali Elsokary on 22/05/2023.
+//  
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+}
