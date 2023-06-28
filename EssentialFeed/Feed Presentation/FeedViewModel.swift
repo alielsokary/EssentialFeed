2df25e9b588e93727051e3e552b2b7d854fba3cb
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-06-28 03:27:19 +0300

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ali Elsokary on 28/06/2023.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
