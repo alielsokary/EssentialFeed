a932d1afc325dcada1fc861f70dee5ca30149c55
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-07-01 19:08:14 +0300

    Make `RemoteFeedImageDataLoader` conform to `FeedImageDataLoader`

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -7,46 +7,50 @@
 
 import Foundation
 
-class RemoteFeedImageDataLoader {
+import Foundation
+
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     private let client: HTTPClient
-    
-    init(client: HTTPClient) {
+
+    public init(client: HTTPClient) {
         self.client = client
     }
-    
+
     public enum Error: Swift.Error {
         case invalidData
     }
-    
+
     private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
+
         var wrapped: HTTPClientTask?
-        
+
         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
             self.completion = completion
         }
-        
+
         func complete(with result: FeedImageDataLoader.Result) {
             completion?(result)
         }
-        
+
         func cancel() {
             preventFurtherCompletions()
             wrapped?.cancel()
         }
+
         private func preventFurtherCompletions() {
             completion = nil
         }
     }
-    
+
     @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         let task = HTTPClientTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
+
             switch result {
-            case let .success((data, response)):
+            case let .success(data, response):
                 if response.statusCode == 200, !data.isEmpty {
                     task.complete(with: .success(data))
                 } else {
