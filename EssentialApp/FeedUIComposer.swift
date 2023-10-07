2f11c2f7b2384bd52a857cc1f915b5c57c72018e
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-10-07 21:53:59 +0300

    Replace FeedPresenter with generic presenter

--- EssentialApp/FeedUIComposer.swift
+++ EssentialApp/FeedUIComposer.swift
@@ -24,11 +24,11 @@ public final class FeedUIComposer {
             delegate: presentationAdapter,
             title: FeedPresenter.title)
         
-        presentationAdapter.presenter = FeedPresenter(
-            feedView: FeedViewAdapter(
+        presentationAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(
                 controller: feedController,
                 imageLoader: imageLoader ),
-            loadingView: WeakRefVirtualProxy(feedController), errorView: WeakRefVirtualProxy(feedController))
+            loadingView: WeakRefVirtualProxy(feedController), errorView: WeakRefVirtualProxy(feedController), mapper: FeedPresenter.map)
         return feedController
     }
 }
