1e41a5326e386f39e59f4273b5405e7ec8a3452a
Ali Elsokary <ali.elsokary.w@gmail.com>
2023-10-10 22:52:44 +0300

    Render comments

--- EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -16,38 +16,38 @@ extension ListViewController {
         
         tableView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
     }
-
-	func simulateUserInitiatedReload() {
-		refreshControl?.simulatePullToRefresh()
-	}
-	
-	@discardableResult
-	func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-		return feedImageView(at: index) as? FeedImageCell
-	}
-	
+    
+    func simulateUserInitiatedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
     @discardableResult
-	func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
-		let view = simulateFeedImageViewVisible(at: row)
-		let delegate = tableView.delegate
-		let index = IndexPath(row: row, section: feedImagesSection)
-		delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
+        return feedImageView(at: index) as? FeedImageCell
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
+        let view = simulateFeedImageViewVisible(at: row)
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
         return view
-	}
-	
-	func simulateFeedImageViewNearVisible(at row: Int) {
-		let ds = tableView.prefetchDataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		ds?.tableView(tableView, prefetchRowsAt: [index])
-	}
-	
-	func simulateFeedImageViewNotNearVisible(at row: Int) {
-		simulateFeedImageViewNearVisible(at: row)
-		
-		let ds = tableView.prefetchDataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-	}
+    }
+    
+    func simulateFeedImageViewNearVisible(at row: Int) {
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView(tableView, prefetchRowsAt: [index])
+    }
+    
+    func simulateFeedImageViewNotNearVisible(at row: Int) {
+        simulateFeedImageViewNearVisible(at: row)
+        
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
     
     func renderedFeedImageData(at index: Int) -> Data? {
         return simulateFeedImageViewVisible(at: index)?.renderedImage
@@ -60,10 +60,45 @@ extension ListViewController {
     var errorMessage: String? {
         return errorView.message
     }
-	
-	var isShowingLoadingIndicator: Bool {
-		return refreshControl?.isRefreshing == true
-	}
+    
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+}
+ 
+extension ListViewController {
+    
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+    }
+    
+    private var commentsSection: Int {
+        return 0
+    }
+    
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+    
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+    
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel .text
+    }
+    
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+}
+
+extension ListViewController {
 	
 	func numberOfRenderedFeedImageViews() -> Int {
         tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
